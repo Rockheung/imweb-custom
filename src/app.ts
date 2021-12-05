88f2ac64c4d23c419e99547f8a46fdf7baa14438
Rockheung <rockheung@gmail.com>
2021-12-05 21:31:10 +0900

    prove async

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@
 console.log(import.meta.webpack); // without reference declared above, TypeScript will throw an error
 
 import * as _ from "lodash";
+import { mul } from "./lib";
 
 console.log("hello", window.location.pathname, window.location.search);
-const a = new Promise((res) => setTimeout(res, 3000));
+const a = (v: any) => new Promise((res) => setTimeout(() => res(v), 3000));
 
 async function main() {
-  throw _.filter([5, 6, 7, 2, 5, 8, 9], (i) => i > 3);
+  const sub = _.filter([5, 6, 7, 2, 5, 8, 9], (i) => i > 3);
+  a(sub.reduce(mul)).then(console.log);
 }
 
 main().catch(console.error);
